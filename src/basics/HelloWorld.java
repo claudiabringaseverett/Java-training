b5e3413a745f75029e32b5c9f87b43d41cac0b2b
Claudia Bringas <40345934+claudiabringaseverett@users.noreply.github.com>
2019-05-26 20:47:12 -0400

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Adding more code to the project");
+		System.out.println("Adding code from Github");
 		
 	}	
 }
